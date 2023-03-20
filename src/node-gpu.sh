d8d8d4839882f2b9f0c6dfe7179492a758d8d894
Leirof <vince.lrf@gmail.com>
2023-03-20 16:58:11 +0100

    🧠 First simple model (not tested)

--- src/node-gpu.sh
+++ src/node-gpu.sh
@@ -17,6 +17,6 @@ echo $NSLOTS
 source ~/M2-Prestel-state-from-obs-ML/venv/bin/activate
 ls -s /scratch-local/vforiel ./M2-Prestel-state-from-obs-ML/scratch-local
 
-~/M2-Prestel-state-from-obs-ML/venv/bin/python ~/M2-Prestel-state-from-obs-ML/venv/bin/jupyter-lab --no-browser --port 1235 --IdentityProvider.token=c7b927e4bd9ad9f008a2491c40b8f5d4790945a935674a6e --ServerApp.allow_origin='*'
+~/M2-Prestel-state-from-obs-ML/venv/bin/python ~/M2-Prestel-state-from-obs-ML/venv/bin/jupyter-lab --no-browser --port 1230 --IdentityProvider.token=c7b927e4bd9ad9f008a2491c40b8f5d4790945a935674a6e --ServerApp.allow_origin='*'
 
 exit 0
